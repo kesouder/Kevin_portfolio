091a3798aa5ef5be8c091280691f7468169ce0b3
Kevin Souder <129797279+kesouder@users.noreply.github.com>
2025-01-22 13:34:10 -0800

    2.1 and 2.2 attempt

--- global.js
+++ global.js
@@ -3,3 +3,10 @@ console.log('IT’S ALIVE!');
 function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
+
+let navLinks = $$("nav a");
+console.log(navLinks);
+
+let currentLink = navLinks.find(
+    (a) => a.host === location.host && a.pathname === location.pathname
+  );
