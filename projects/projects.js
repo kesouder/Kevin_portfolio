3c1f11c9ec63d7d53425d8c1710b9d68b4750f23
Kevin Souder <129797279+kesouder@users.noreply.github.com>
2025-01-29 15:39:58 -0800

    1.4 test

--- projects/projects.js
+++ projects/projects.js
@@ -1,2 +1,22 @@
 import { fetchJSON, renderProjects } from '../global.js';
 
+// const projects = await fetchJSON('../lib/projects.json');
+// const projectsContainer = document.querySelector('.projects');
+// renderProjects(projects, projectsContainer, 'h2');
+
+document.addEventListener('DOMContentLoaded', async () => {
+    // Fetch the JSON data
+    const projects = await fetchJSON('../lib/projects.json');
+    if (projects) {
+        console.log('Projects were fetched', projects);
+        const projectsContainer = document.querySelector('.projects');
+        if (projectsContainer) {
+            renderProjects(projects, projectsContainer, 'h2');
+            console.log('Projects rendered successfully:', projectsContainer);
+        } else {
+            console.error('Container element not found');
+        }
+    } else {
+        console.error('No data fetched');
+    }
+});
