132c3ed9921255d00769dbca617fb5601c6315ef
Kevin Souder <129797279+kesouder@users.noreply.github.com>
2025-01-29 17:01:15 -0800

    added new func to import for index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { fetchJSON, renderProjects } from './global.js';
+import { fetchJSON, renderProjects, fetchGitHubData } from './global.js';
 
 document.addEventListener('DOMContentLoaded', async () => {
 
