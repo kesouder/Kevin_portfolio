30b447e3fc947ef5febc1d99c69e10ef8670476f
Kevin Souder <129797279+kesouder@users.noreply.github.com>
2025-02-04 22:11:54 -0800

    1.2 and 1.3

--- projects/projects.js
+++ projects/projects.js
@@ -1,4 +1,5 @@
 import { fetchJSON, renderProjects } from '../global.js';
+import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm";
 
 document.addEventListener('DOMContentLoaded', async () => {
     const projects = await fetchJSON('../lib/projects.json');
@@ -37,4 +38,16 @@ document.addEventListener('DOMContentLoaded', async () => {
     } else {
         console.error('No data fetched');
     }
+
+    // lab 5 D3
+    const svg = d3.select("#projects-pie-plot");
+    let arc = d3.arc().innerRadius(0).outerRadius(50);({
+        startAngle: 0,
+        endAngle: 2 * Math.PI
+    });
+
+    d3.select('svg').append("path")
+        .attr("d", arc)
+        .attr("fill", "red");
+
 });
